e0cdad2ab1917cc9df46c3b6e4b48a77f0c73312
ZpFate <zptwistedfate@163.com>
2019-07-04 08:52:22 +0800

    修改README文件 以及shell脚本

--- ArchiveAutomically.sh
+++ ArchiveAutomically.sh
@@ -1,27 +1,90 @@
-#!/bin/bash
+#!/bin/zsh
 
-Project_Path=$( cd "$( dirname $0)" & pwd)
+# 获取当前目录, 请将脚本放置于workspace同级
+PROJECT_PATH="$( cd "$( dirname "$0"  )" && pwd  )"
 
-# echo $Project_Path
+# 项目target名字
+TARGET_NAME=$1
 
-Project_Name=ArchieveAutomiclly
-Target_Name=ArchieveAutomiclly
-
-xcodebuild clean -configuration "$configuration" -target "$Project_Name"
+# 打包环境
+CONFIGURATION=Release
 
 #工程文件路径
-APP_PATH="${Project_Path}/${Project_Name}.xcodeproj"
+# APP_PATH="${PROJECT_PATH}.xcodeproj"  #普通工程
+APP_PATH="${PROJECT_PATH}/${TARGET_NAME}.xcworkspace"   #工作区
+
+# Xcode clean
+xcodebuild clean -workspace "${APP_PATH}" -configuration "${CONFIGURATION}" -scheme "${TARGET_NAME}"
+
+# 打包目录
+DESKTOP_PATH="~/Desktop"
+
+# 时间戳
+CURRENT_TIME=$(date "+%Y-%m-%d %H-%M-%S")
 
 # 归档路径
-ARCHIVE_PATH="${Project_Path}/archive/${Target_Name}.xcarchive"
+ARCHIVE_PATH="${DESKTOP_PATH}/${TARGET_NAME} ${CURRENT_TIME}/${TARGET_NAME}.xcarchive"
 # 编译环境
-CONFIGURATION=Debug
+  
 # 导出路径
-EXPORT_PATH="${Project_Path}/archive/${Target_Name}"
-# plist路径
-PLIST_PATH="${Project_Path}/ExportOptions.plist"
+EXPORT_PATH="${DESKTOP_PATH}/${TARGET_NAME} ${CURRENT_TIME}"
 
-# archive
-xcodebuild archive -project "${APP_PATH}" -scheme "${Target_Name}" -configuration "${CONFIGURATION}" -archivePath "${ARCHIVE_PATH}" 
-# 导出ipa
+# plist路径
+PLIST_PATH="${PROJECT_PATH}/ExportOptions.plist"
+
+# archive 这边使用的工作区间 也可以使用project
+xcodebuild archive -workspace "${APP_PATH}" -scheme "${TARGET_NAME}" -configuration "${CONFIGURATION}" -archivePath "${ARCHIVE_PATH}" 
+
+# # 导出ipa
 xcodebuild -exportArchive -archivePath "${ARCHIVE_PATH}" -exportPath "${EXPORT_PATH}" -exportOptionsPlist "${PLIST_PATH}"
+
+
+
+
+
+
+
+
+#!/bin/zsh
+# PROJECT_PATH=$1
+# echo $PROJECT_PATH
+
+# TARGET_NAME=$2
+
+# CONFIGURATION=$3
+# echo $CONFIGURATION
+
+# if ! "$CONFIGURATION"; then
+#     $CONFIGURATION=Build
+# fi
+# echo $CONFIGURATION
+
+#工程文件路径
+# APP_PATH="${PROJECT_PATH}.xcodeproj"  #普通工程
+# # APP_PATH="${PROJECT_PATH}.xcworkspace"   #工作区
+
+# # com.TwistedFate.www.ArchieveAutomic
+
+# INFO_PATH="/Users/twistedfate/Desktop/TwistedFate/BlogDemo/ArchieveAutomiclly/ArchieveAutomiclly.xcodeproj/project.pbxproj"
+
+# /usr/libexec/PlistBuddy -c 'Set :CFBundleIdentifier "com.TwistedFate.www.ArchieveAutomic" ' ${INFO_PATH}
+
+# xcodebuild clean -project "${PROJECT_PATH}" -configuration "$configuration" -target "${TARGET_NAME}"
+
+# # 归档路径
+# ARCHIVE_PATH="${PROJECT_PATH}/archive/${TARGET_NAME}.xcarchive"
+# # 编译环境
+  
+
+# # 导出路径
+# EXPORT_PATH="${PROJECT_PATH}/archive/${TARGET_NAME}"
+# # plist路径
+# PLIST_PATH="${PROJECT_PATH}/ExportOptions.plist"
+
+# # archive
+# xcodebuild archive -project "${APP_PATH}" -scheme "${TARGET_NAME}" -configuration "${CONFIGURATION}" -archivePath "${ARCHIVE_PATH}" 
+# # 导出ipa
+# xcodebuild -exportArchive -archivePath "${ARCHIVE_PATH}" -exportPath "${EXPORT_PATH}" -exportOptionsPlist "${PLIST_PATH}"
+
+
+
--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-#ArchiveAutomatically-shell
+# 使用
+
+将脚本放置到project或者workspace同级目录, 将脚本拖入终端 在空格后输入要打包工程的target名
